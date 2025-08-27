2b98d5e2768b3259a14de7f716922450dd8df2d7
Muhammad-Khizar-Waqar <116596548+Muhammad-Khizar-Waqar@users.noreply.github.com>
2025-08-27 10:15:06 +0500

    text update

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,86 +1,159 @@
-import Marquee from 'react-fast-marquee';
+import Marquee from "react-fast-marquee";
 
 export default function Hero() {
-    return (
-        <>
-            <section id="about" className="section-hero-2 position-relative pt-130 pb-3">
-                <div className="container hero-2">
-                    <div className="border border-1 rounded-3">
-                        <div className="box-linear-animation position-relative z-1">
-                            <div className="row align-items-end py-60">
-                                <div className="col-lg-5 ps-lg-5 text-lg-start text-center">
-                                    <div className="position-relative mb-lg-0 mb-5">
-                                        <img src="assets/imgs/home-page-2/hero-1/people.png" alt="khizar" />
-                                        <div className="position-absolute end-0 top-100 translate-middle-y icon-decorate">
-                                            <img src="assets/imgs/home-page-2/hero-1/icon.svg" alt="khizar" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 mx-lg-auto col-md-12">
-                                    <div className="p-lg-0 p-md-8 p-3">
-                                        <div className="text-secondary-2 d-flex align-items-center">
-                                            &lt;span&gt;
-                                            <div className="text-dark">
-                                                <div className="typewriter">
-                                                    <h1 className="fs-6 fw-medium">Hey, I’m Khizar</h1>
-                                                </div>
-                                            </div>
-                                            &lt;/span&gt;
-                                        </div>
-                                        <h1 className="fs-50 my-3">Senior <span className="text-linear-4">{'{'}Full Stack{'}'}</span>Web &amp; App developer<span className="flicker">_</span></h1>
-                                        <p className="mb-6 text-secondary-2">&lt;p&gt;<span className="text-dark">With expertise in cutting-edge technologies such as</span> <span className="text-secondary-2">NodeJS</span>, <span className="text-secondary-2">React</span>, <span className="text-secondary-2">Angular</span>, and <span className="text-secondary-2">Laravel</span><span className="text-dark">... I deliver web solutions that are both innovative and robust.</span>&lt;/p&gt;</p>
-                                        <div className="row">
-                                            <div className="col-7">
-                                                {/* Carausel Scroll */}
-                                                <Marquee className="carouselTicker carouselTicker-left position-relative z-1 mt-lg-0 mt-8">
-                                                    <ul className="carouselTicker__list ">
-                                                        <li className="carouselTicker__item">
-                                                            <a href="#" className="brand-logo icon_60 icon-shape rounded-3">
-                                                                <img src="assets/imgs/home-page-2/hero-1/icon-1.svg" alt="brand" />
-                                                            </a>
-                                                        </li>
-                                                        <li className="carouselTicker__item">
-                                                            <a href="#" className="brand-logo icon_60 icon-shape rounded-3">
-                                                                <img src="assets/imgs/home-page-2/hero-1/icon-2.svg" alt="brand" />
-                                                            </a>
-                                                        </li>
-                                                        <li className="carouselTicker__item">
-                                                            <a href="#" className="brand-logo icon_60 icon-shape rounded-3">
-                                                                <img src="assets/imgs/home-page-2/hero-1/icon-3.svg" alt="brand" />
-                                                            </a>
-                                                        </li>
-                                                        <li className="carouselTicker__item">
-                                                            <a href="#" className="brand-logo icon_60 icon-shape rounded-3">
-                                                                <img src="assets/imgs/home-page-2/hero-1/icon-4.svg" alt="brand" />
-                                                            </a>
-                                                        </li>
-                                                        <li className="carouselTicker__item">
-                                                            <a href="#" className="brand-logo icon_60 icon-shape rounded-3">
-                                                                <img src="assets/imgs/home-page-2/hero-1/icon-5.svg" alt="brand" />
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </Marquee>
-                                            </div>
-                                            <div className="col-5 d-flex align-items-end">
-                                                <span className="fs-6 text-300 mb-2">...and more</span>
-                                            </div>
-                                        </div>
-                                        <a href="assets/resume.pdf" className="btn me-2 text-300 ps-0 mt-4" target="_blank" rel="noopener noreferrer">
-                                            <i className="ri-download-line text-primary-2" />
-                                            [ Download my CV ]
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <>
+      <section
+        id="about"
+        className="section-hero-2 position-relative pt-130 pb-3"
+      >
+        <div className="container hero-2">
+          <div className="border border-1 rounded-3">
+            <div className="box-linear-animation position-relative z-1">
+              <div className="row align-items-end py-60">
+                <div className="col-lg-5 ps-lg-5 text-lg-start text-center">
+                  <div className="position-relative mb-lg-0 mb-5">
+                    <img
+                      src="assets/imgs/home-page-2/hero-1/people.png"
+                      alt="khizar"
+                    />
+                    <div className="position-absolute end-0 top-100 translate-middle-y icon-decorate">
+                      <img
+                        src="assets/imgs/home-page-2/hero-1/icon.svg"
+                        alt="khizar"
+                      />
                     </div>
+                  </div>
                 </div>
-                <div className="background position-absolute top-0 start-0 w-100 h-100">
-                    <img className="bg-w" src="assets/imgs/home-page-2/hero-1/bg.png" alt="khizar" />
-                    <img className="bg-d" src="assets/imgs/home-page-2/hero-1/bg-dark.png" alt="khizar" />
+                <div className="col-lg-6 mx-lg-auto col-md-12">
+                  <div className="p-lg-0 p-md-8 p-3">
+                    <div className="text-secondary-2 d-flex align-items-center">
+                      &lt;span&gt;
+                      <div className="text-dark">
+                        <div className="typewriter">
+                          <h1 className="fs-6 fw-medium">Hey, I’m Khizar</h1>
+                        </div>
+                      </div>
+                      &lt;/span&gt;
+                    </div>
+                    <h1 className="fs-50 my-3">
+                      Senior{" "}
+                      <span className="text-linear-4">
+                        {"{"}Full Stack{"}"}
+                      </span>{" "}
+                      Web developer<span className="flicker">_</span>
+                    </h1>
+                    <p className="mb-6 text-secondary-2">
+                      &lt;p&gt;
+                      <span className="text-dark">
+                        With expertise in cutting-edge technologies such as
+                      </span>{" "}
+                      <span className="text-secondary-2">NodeJS</span>,{" "}
+                      <span className="text-secondary-2">React JS</span>,{" "}
+                      <span className="text-secondary-2">Next JS</span>,{" "}
+                      <span className="text-secondary-2">Vue JS</span>, and{" "}
+                      <span className="text-secondary-2">Mongoose DB</span>
+                      <span className="text-dark">
+                        ... I deliver web solutions that are both innovative and
+                        robust.
+                      </span>
+                      &lt;/p&gt;
+                    </p>
+                    <div className="row">
+                      <div className="col-7">
+                        {/* Carausel Scroll */}
+                        <Marquee className="carouselTicker carouselTicker-left position-relative z-1 mt-lg-0 mt-8">
+                          <ul className="carouselTicker__list ">
+                            <li className="carouselTicker__item">
+                              <a
+                                href="#"
+                                className="brand-logo icon_60 icon-shape rounded-3"
+                              >
+                                <img
+                                  src="assets/imgs/home-page-2/hero-1/icon-1.svg"
+                                  alt="brand"
+                                />
+                              </a>
+                            </li>
+                            <li className="carouselTicker__item">
+                              <a
+                                href="#"
+                                className="brand-logo icon_60 icon-shape rounded-3"
+                              >
+                                <img
+                                  src="assets/imgs/home-page-2/hero-1/icon-2.svg"
+                                  alt="brand"
+                                />
+                              </a>
+                            </li>
+                            <li className="carouselTicker__item">
+                              <a
+                                href="#"
+                                className="brand-logo icon_60 icon-shape rounded-3"
+                              >
+                                <img
+                                  src="assets/imgs/home-page-2/hero-1/icon-3.svg"
+                                  alt="brand"
+                                />
+                              </a>
+                            </li>
+                            <li className="carouselTicker__item">
+                              <a
+                                href="#"
+                                className="brand-logo icon_60 icon-shape rounded-3"
+                              >
+                                <img
+                                  src="assets/imgs/home-page-2/hero-1/icon-4.svg"
+                                  alt="brand"
+                                />
+                              </a>
+                            </li>
+                            <li className="carouselTicker__item">
+                              <a
+                                href="#"
+                                className="brand-logo icon_60 icon-shape rounded-3"
+                              >
+                                <img
+                                  src="assets/imgs/home-page-2/hero-1/icon-5.svg"
+                                  alt="brand"
+                                />
+                              </a>
+                            </li>
+                          </ul>
+                        </Marquee>
+                      </div>
+                      <div className="col-5 d-flex align-items-end">
+                        <span className="fs-6 text-300 mb-2">...and more</span>
+                      </div>
+                    </div>
+                    <a
+                      href="assets/resume.pdf"
+                      className="btn me-2 text-300 ps-0 mt-4"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="ri-download-line text-primary-2" />[
+                      Download my CV ]
+                    </a>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="background position-absolute top-0 start-0 w-100 h-100">
+          <img
+            className="bg-w"
+            src="assets/imgs/home-page-2/hero-1/bg.png"
+            alt="khizar"
+          />
+          <img
+            className="bg-d"
+            src="assets/imgs/home-page-2/hero-1/bg-dark.png"
+            alt="khizar"
+          />
+        </div>
+      </section>
+    </>
+  );
 }
